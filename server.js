2159846990206cca4fa6ec00625db6df6515791a
Dustin Tierney <dustintierney@rockethomes.com>
2022-10-18 14:49:04 -0400

    updated the all employee query

--- server.js
+++ server.js
@@ -53,7 +53,8 @@ const prompt = () => {
 };
 
 function viewAllEmployees() {
-    let sql = "SELECT employee.id, employee.first_name, employee.last_Name, role.title, department.name, role.salary FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id";
+    let sql =
+        "SELECT e.id, e.first_name, e.last_Name, role.title, department.name AS department, role.salary, CONCAT(m.first_name, ' ', m.first_name) as manager FROM employee e JOIN role ON e.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee m ON m.id = e.manager_id ORDER BY e.id ASC";
 
     db.query(sql, (error, results) => {
         if (error) {
@@ -95,7 +96,7 @@ function addEmployee() {
                 if (error) {
                     console.log(error);
                 }
-                console.log('The employee has been successfully added!');
+                console.log("The employee has been successfully added!");
                 prompt();
             });
         });
@@ -103,29 +104,29 @@ function addEmployee() {
 
 function updateEmployeeRole() {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What is the id of the employee you would like to update?",
-            name: "employeeId",
-        },
-        {
-            type: "input",
-            message: "What is the id of the employee's new role?",
-            name: "roleId",
-        },
-    ])
-    .then((answers) => {
-        let sql = `UPDATE employee SET role_id = ${answers.roleId} WHERE id = ${answers.employeeId})`;
+        .prompt([
+            {
+                type: "input",
+                message: "What is the id of the employee you would like to update?",
+                name: "employeeId",
+            },
+            {
+                type: "input",
+                message: "What is the id of the employee's new role?",
+                name: "roleId",
+            },
+        ])
+        .then((answers) => {
+            let sql = `UPDATE employee SET role_id = ${answers.roleId} WHERE id = ${answers.employeeId})`;
 
-        db.query(sql, (error, results) => {
-            if (error) {
-                console.error(error);
-            }
-            console.table(results);
-            prompt();
+            db.query(sql, (error, results) => {
+                if (error) {
+                    console.error(error);
+                }
+                console.table(results);
+                prompt();
+            });
         });
-    });
 }
 
 function viewAllRoles() {
@@ -142,34 +143,34 @@ function viewAllRoles() {
 
 function addRole() {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What is the title of the role?",
-            name: "title",
-        },
-        {
-            type: "input",
-            message: "What is the salary of the role?",
-            name: "salary",
-        },
-        {
-            type: "input",
-            message: "What is the id of the role's department?",
-            name: "departmentId",
-        },
-    ])
-    .then((answers) => {
-        let sql = `INSERT INTO role (title, salary, department_id) VALUES (${answers.title}, ${answers.salary}, ${answers.departmentId})`;
+        .prompt([
+            {
+                type: "input",
+                message: "What is the title of the role?",
+                name: "title",
+            },
+            {
+                type: "input",
+                message: "What is the salary of the role?",
+                name: "salary",
+            },
+            {
+                type: "input",
+                message: "What is the id of the role's department?",
+                name: "departmentId",
+            },
+        ])
+        .then((answers) => {
+            let sql = `INSERT INTO role (title, salary, department_id) VALUES (${answers.title}, ${answers.salary}, ${answers.departmentId})`;
 
-        db.query(sql, (error, results) => {
-            if (error) {
-                console.error(error);
-            }
-            console.log('The role has been successfully added!');
-            prompt();
+            db.query(sql, (error, results) => {
+                if (error) {
+                    console.error(error);
+                }
+                console.log("The role has been successfully added!");
+                prompt();
+            });
         });
-    });
 }
 
 function viewAllDepartments() {
@@ -186,24 +187,24 @@ function viewAllDepartments() {
 
 function addDepartment() {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What is the name of the department?",
-            name: "name",
-        },
-    ])
-    .then((answers) => {
-        let sql = `INSERT INTO role (name) VALUES (${answers.name})`;
+        .prompt([
+            {
+                type: "input",
+                message: "What is the name of the department?",
+                name: "name",
+            },
+        ])
+        .then((answers) => {
+            let sql = `INSERT INTO role (name) VALUES (${answers.name})`;
 
-        db.query(sql, (error, results) => {
-            if (error) {
-                console.error(error);
-            }
-            console.log('The department has been successfully added!');
-            prompt();
+            db.query(sql, (error, results) => {
+                if (error) {
+                    console.error(error);
+                }
+                console.log("The department has been successfully added!");
+                prompt();
+            });
         });
-    });
 }
 
 function quit() {
